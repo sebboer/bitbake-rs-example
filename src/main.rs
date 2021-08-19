77935cfc60d81cd26440e0de77c481b219070448
Sebastian Boerakker <sebboer@protonmail.com>
2021-08-19 13:31:21 +0200

    make client a daemon

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::collections::HashMap;
+use tokio::time;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
-    Ok(())
+    let mut interval = time::interval(time::Duration::from_secs(10));
+
+    loop {
+        interval.tick().await;
+        let resp = reqwest::get("https://httpbin.org/ip")
+            .await?
+            .json::<HashMap<String, String>>()
+            .await?;
+        println!("{:#?}", resp);
+    }
 }
